d75de8e2cf5f8f86a9b4f2b95a32052064776a53
Olli Wilkman <olli.wilkman@iki.fi>
2018-11-22 15:30:24 +0200

    Fix bug

--- src/Bezier.jl
+++ src/Bezier.jl
@@ -1,4 +1,3 @@
-
 module Bezier
 
 export BezierCurve
@@ -8,13 +7,13 @@ struct BezierCurve
     Ycoef::Vector{Float64}
 end
 
-function Bezier(r0::Vector, r1::Vector, t0::Vector, t1::Vector)
+function BezierCurve(r0::Vector, r1::Vector, t0::Vector, t1::Vector)
     X = bezier_coefs(r0[1], r1[1], t0[1], t1[1])
     Y = bezier_coefs(r0[2], r1[2], t0[2], t1[2])
-    return Bezier(X, Y)
+    return BezierCurve(X, Y)
 end
 
-function (curve::Bezier)(t::Real)
+function (curve::BezierCurve)(t::Real)
     out = zeros(2)
     for i = 1:4
         out[1] += curve.Xcoef[i] * t^(4-i)
